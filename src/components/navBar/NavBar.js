45c296f3d818e71441ff0c10b8e8fc4ddb21b7da
FacundoVillarroel <facu.villarroel96@gmail.com>
2023-02-13 13:21:09 -0300

    [setup] - added fonts, colors variables and bootstrap

---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NavBar() {
+
+
+  return (
+    <div>
+      NavBar
+      <p className='textBeautyDream'>BeautyDreams</p>
+      <p className='textLovelo'>Lovelo</p>
+      <p className='textDosis'>Dosis</p>
+    </div>
+  )
+}
+
+export default NavBar
