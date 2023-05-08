5a0f3d73ae0334b0786e59a4208b67af0014a87a
FacundoVillarroel <facu.villarroel96@gmail.com>
2023-05-08 20:31:19 +1000

    [feature] - Blog base structure

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,32 +4,48 @@ const Blog = () => {
   return (
     <div className='blogContainer'>
       <p className='title'>
-        Blog
+        Nuestro Blog
       </p>
       <div className='blogGrid'>
-        <div className='articleContainer'>
-          
+        <div className='articleContainer article1'>
+          <h2>
+            MIS EXPECTATIVAS EN PSICOTERAPIA
+          </h2>
         </div>
-        <div className='articleContainer'>
-          
+        <div className='articleContainer article2'>
+          <h2>
+            ¿QUÉ ES LA PSICOLOGIA?
+          </h2>
         </div>
-        <div className='articleContainer'>
-          
+        <div className='articleContainer article3'>
+          <h2>
+            ANSIEDAD: TERAPIA COGNITIVA AFECTIVA
+          </h2>
         </div>
-        <div className='articleContainer'>
-          
+        <div className='articleContainer article4'>
+          <h2>
+            CRIANZA Y DESARROLLO EMOCIONAL
+          </h2>
         </div>
-        <div className='articleContainer'>
-          
+        <div className='articleContainer article5'>
+          <h2>
+            PSICOLOGÍA POSITIVA PARA BIENESTAR
+          </h2>
         </div>
-        <div className='articleContainer'>
-          
+        <div className='articleContainer article6'>
+          <h2>
+            RESILIENCIA: SUPERAR TRAUMAS
+          </h2>
         </div>
-        <div className='articleContainer'>
-          
+        <div className='articleContainer article7'>
+          <h2>
+            DEPRESIÓN: ENFOQUES PSICOLÓGICOS
+          </h2>
         </div>
-        <div className='articleContainer'>
-          
+        <div className='articleContainer article8'>
+          <h2>
+            RESILIENCIA: SUPERANDO TRAUMAS
+          </h2>
         </div>
       </div>
     </div>
