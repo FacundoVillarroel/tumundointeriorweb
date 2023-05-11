a5621d0d18be479128c42ee3b54e6ba502c18bf9
FacundoVillarroel <facu.villarroel96@gmail.com>
2023-05-11 21:11:02 +1000

    [feature] - border radius to image section 1 & tu mundo interior string in frequent questions in font style italic

--- src/components/FrequentQuestion/FrequentQuestion.js
+++ src/components/FrequentQuestion/FrequentQuestion.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import {Accordion, Container} from "react-bootstrap";
+import { Accordion, Container } from 'react-bootstrap';
 
-const FrequentQuestion = ({question}) => {
+const FrequentQuestion = ({ question }) => {
+  const italicStyle = {
+    fontStyle: 'italic'
+  };
+
+  const italicizeText = (text) => {
+    const targetText = 'tu mundo interior';
+    const regex = new RegExp(targetText, 'gi');
+    return text.replace(regex, `<span style="font-style: italic;">${targetText}</span>`);
+  };
+
+  const formattedQuestion = italicizeText(question.question);
+  const formattedAnswer = italicizeText(question.answer);
 
   return (
     <Container>
-            <Accordion.Item eventKey={question.question} bsPrefix="question">
-                <Accordion.Header>
-                  <div className="questionContainer">
-                    <p className='questionText'>
-                      {question.question}
-                    </p>
-                  </div>
-                </Accordion.Header>
-                <Accordion.Body>
-                    <div className="answerContainer">
-                        <p className='answerText'>
-                          {question.answer}
-                        </p>
-                    </div>
-                </Accordion.Body>
-            </Accordion.Item>
-        </Container>
-  )
-}
+      <Accordion.Item eventKey={question.question} bsPrefix="question">
+        <Accordion.Header>
+          <div className="questionContainer">
+            <p className='questionText' dangerouslySetInnerHTML={{ __html: formattedQuestion }}></p>
+          </div>
+        </Accordion.Header>
+        <Accordion.Body>
+          <div className="answerContainer">
+            <p className='answerText' dangerouslySetInnerHTML={{ __html: formattedAnswer }}></p>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Container>
+  );
+};
 
-export default FrequentQuestion
+export default FrequentQuestion;
